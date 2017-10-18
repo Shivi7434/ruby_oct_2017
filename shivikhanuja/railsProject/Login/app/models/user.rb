ddc252f8cb9c5fdacc7f5be5bf5f5fdddf572922
Shivi7434 <shivalikhanuja.net@gmail.com>
2017-10-17 19:54:39 -0500

    updating login registration

---
+++ shivikhanuja/railsProject/Login/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  email_regex = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+
+  before_save :downcase
+
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness:true, format: { with: email_regex}
+
+  def downcase
+      self.first_name.downcase!
+      self.last_name.downcase!
+      self.email.downcase!
+  end  
+end
--- shivikhanuja/railsProject/Login/config/routes.rb
+++ shivikhanuja/railsProject/Login/config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+    root 'users#new'
+  
+    #New User
+    get 'register' => 'users#new', as: 'new_register'
+    get'users/new' => 'users#new'
+    #create user
+    post 'register' => 'users#create', as: 'register'
+    post 'users' => 'users#create'
+    #show user
+    get 'users/:id' => 'users#show', as: 'user'
+    # New Login
+    get 'login' => 'logins#new', as:'new_login'
+    get 'logins/new' => 'logins#new'
+    # Login
+    post 'login' => 'logins#create' ,as: 'login'
+    post 'logins'  => 'logins#create'
+    # Logout   
+    delete 'logout' => 'logins#destroy', as: 'logout'
+    delete 'logins' => 'logins#destroy' 
+  
+  
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  
